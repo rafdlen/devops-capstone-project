c03517cdb9ad43345e680cad6561dfcccad18fa5
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2022-11-13 01:49:30 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -30,7 +30,7 @@ def index():
 
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
-  """
+    """
   Creates an Account
   This endpoint will create an Account based the data in the body that is posted
   """
@@ -50,20 +50,20 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-  """
+    """
   List all Accounts
   This endpoint will list all Accounts
   """
-   app.logger.info("Request to list Accounts")
-   accounts = Account.all()
-   account_list = [account.serialize() for account in accounts]
-   app.logger.info("Returning [%s] accounts", len(account_list))
-   return jsonify(account_list), status.HTTP_200_OK
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-  """
+    """
   Reads an Account
   This endpoint will read an Account based the account_id that is requested
   """
@@ -78,7 +78,7 @@ def get_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-  """
+    """
   Update an Account
   This endpoint will update an Account based on the posted data
   """
