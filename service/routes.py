206223e1fb0bccfbb6e9e57df06eda4b9a5c4160
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2022-11-13 01:32:45 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -77,6 +77,7 @@ def list_accounts():
     # READ AN ACCOUNT
     ######################################################################
 
+    
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -95,6 +96,7 @@ def get_accounts(account_id):
     # UPDATE AN EXISTING ACCOUNT
     ######################################################################
 
+    
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -113,6 +115,7 @@ def update_accounts(account_id):
     # DELETE AN ACCOUNT
     ######################################################################
 
+    
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
