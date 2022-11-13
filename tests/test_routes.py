2683ff14865342d210bb9f1276e87c2c31addb9e
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2022-11-13 14:24:40 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,6 +19,7 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -101,33 +102,30 @@ class TestAccountService(TestCase):
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-#     def test_unsupported_media_type(self):
-#         """It should not Create an Account when sending the wrong media type"""
-#         account = AccountFactory()
-#         response = self.client.post(
-#             BASE_URL,
-#             json=account.serialize(),
-#             content_type="test/html"
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-        
+    def test_unsupported_media_type(self):
+        """It should not Create an Account when sending the wrong media type"""
+        account = AccountFactory()
+        response = self.client.post(
+            BASE_URL,
+            json=account.serialize(),
+            content_type="test/html"
+        )
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-      def test_get_account(self):
-          """It should Read a single Account"""
-          account = self._create_accounts(1)[0]
-          resp = self.client.get(
-              f"{BASE_URL}/{account.id}", content_type="application/json"
-          )
-          self.assertEqual(resp.status_code, status.HTTP_200_OK)
-          data = resp.get_json()
-          self.assertEqual(data["name"], account.name)
-
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
 
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
@@ -136,7 +134,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -153,13 +150,11 @@ class TestAccountService(TestCase):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
 
-
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
